22f0b6b40da6247fc2b1a89f2ebf1f1133171f2b
Berzan3 <63124461+Berzan3@users.noreply.github.com>
2020-08-20 22:04:43 +0200

    added a grid and some of the material for the project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 function Projects() {
   return (
-    <div className = "Projects">
+    <div className = "Container">
         <title>PROJECTS</title>
-      <p>Projects page :)</p>
-
+        <div className = "Project"><img src="../img/gifmedgranatkast.gif" alt="SignalParadox"/>
+        <h3>SignalParadox</h3>
+        </div>
+        <div className = "Project"><img src="../img/Spaceinvader_c++.gif" alt="Spaceinvader-gif"/>
+        <h3>Spaceinvader/C++</h3>
+        </div>
+        <div className = "Project"><img src="../img/gifmedgranatkast.gif" alt="GameJam"/>
+        <h3>Game Jam</h3>
+        </div>
+        <div className = "Project"><img src="../img/enviroment-UnrealEngine.png" alt="Enviroment-img"/>
+        <h3>An enviroment inside Unreal Engine</h3>
+        </div>
     </div>
   );
 }
