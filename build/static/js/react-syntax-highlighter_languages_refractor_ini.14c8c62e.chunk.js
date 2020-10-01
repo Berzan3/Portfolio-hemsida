f9bf20dc2b849d63ccf5136127edc0d206a5669b
Berzan3 <63124461+Berzan3@users.noreply.github.com>
2020-10-01 15:47:07 +0200

    created a build for githup pages

---
+++ build/static/js/react-syntax-highlighter_languages_refractor_ini.14c8c62e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpvsprojects=this.webpackJsonpvsprojects||[]).push([[60],{189:function(t,s,n){"use strict";function e(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=e,e.displayName="ini",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.14c8c62e.chunk.js.map
