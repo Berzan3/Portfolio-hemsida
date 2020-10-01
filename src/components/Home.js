407b649b4d6a985c78d48a619c53b38a65ba1f3d
Berzan3 <63124461+Berzan3@users.noreply.github.com>
2020-10-01 15:42:43 +0200

    installed gh-pages and trying to deploy to github

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,9 @@ function Home() {
     <div className="Home">
       <title>Andreas Berzelius</title>
       <img src="../img/AndreasBerzelius.png" alt="andreas"></img>
-      <p>Hi my name is Andreas! Im a Computer &#38; Systems Science student with specialization to Computer game development at Stockholm University.
-        Im a eager programmer that loves to learn and work with other people on different projects. I love to make games but im open to make other things too.
+      <p>Hi my name is Andreas! Im a Computer and Systems Science student with specialization to Computer game development at Stockholm University.
+        Im a eager programmer that loves to learn and work with other people on different projects. I love to make games but im open to make other things too. 
+        This website is created by me.
       </p>
     </div>
   );
