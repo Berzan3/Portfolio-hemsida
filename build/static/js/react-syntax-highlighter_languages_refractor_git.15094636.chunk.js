6eefeb71db652eeacdac41d7e0c278da797fc5e0
Berzan3 <63124461+Berzan3@users.noreply.github.com>
2020-10-01 20:48:55 +0200

    Revert "created a build for githup pages"
    This reverts commit f9bf20dc2b849d63ccf5136127edc0d206a5669b.

--- build/static/js/react-syntax-highlighter_languages_refractor_git.15094636.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpvsprojects=this.webpackJsonpvsprojects||[]).push([[45],{174:function(s,t,e){"use strict";function m(s){s.languages.git={comment:/^#.*/m,deleted:/^[-\u2013].*/m,inserted:/^\+.*/m,string:/("|')(?:\\.|(?!\1)[^\\\r\n])*\1/m,command:{pattern:/^.*\$ git .*$/m,inside:{parameter:/\s--?\w+/m}},coord:/^@@.*@@$/m,commit_sha1:/^commit \w{40}$/m}}s.exports=m,m.displayName="git",m.aliases=[]}}]);
-//# sourceMappingURL=react-syntax-highlighter_languages_refractor_git.15094636.chunk.js.map
